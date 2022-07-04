ac7040e9cf2008d70d5aa094cc1a5d70cf4341f2
Lucas Freitas <lucas@xsys.com.br>
2022-07-04 12:13:47 -0300

    types e interfaces

--- script.ts
+++ script.ts
@@ -7,26 +7,49 @@ function soma(a: number, b:number) { //Tipagem de dados na hora da declaração 
 interface IAnimal {
     nome: string;
     tipo: 'terrestre' | 'aquático';
-    executarRugido(decibeis: number): void;
+    // executarRugido(decibeis: number): void;
+    domestico: boolean;
 }
 
 interface IFelino extends IAnimal{
     visaoNoturna:boolean;
 }
 
+interface ICanino extends IAnimal{
+    porte: 'pequeno' | 'medio' | 'grande';
+}
+
+
 const animal: IAnimal = {
     nome: "Everaldo",
     tipo: "terrestre",
-    executarRugido: (decibeis) => console.log((`${decibeis}dB`))
+    domestico: false
+    // executarRugido: (decibeis) => console.log((`${decibeis}dB`))
 }
 
 const felino: IFelino = {
     nome: "Patrício",
     tipo: "aquático",
+    domestico: true,
     visaoNoturna: true,
-    executarRugido: (decibeis) => console.log((`${decibeis}dB`))
+    // executarRugido: (decibeis) => console.log((`${decibeis}dB`))
 }
 
-felino.executarRugido(6);
+//Types 
 
-animal.executarRugido(12);
+type IDomestico = IFelino | ICanino;
+
+const cachorro: IDomestico = {
+    nome: 'Pereira Funk',
+    tipo: 'aquático',
+    domestico: false,
+    porte: 'medio'
+
+}
+
+// felino.executarRugido(6);
+
+// animal.executarRugido(12);
+
+
+
