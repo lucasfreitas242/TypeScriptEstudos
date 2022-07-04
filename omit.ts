06da2e2bd3ad1fb68ed61f9f8caae7619ddba09f
Lucas Freitas <lucas@xsys.com.br>
2022-07-04 14:28:41 -0300

    Omit

---
+++ omit.ts
@@ -0,0 +1,14 @@
+interface Pessoa {
+    nome: string, 
+    idade: number, 
+    nacionalidade: string;
+}
+
+interface Brasileiro extends Omit<Pessoa, 'nacionalidade'> {
+
+}
+
+const brasileiro: Brasileiro = {
+    idade: 35,
+    nome: 'Martin Garrix'
+}
